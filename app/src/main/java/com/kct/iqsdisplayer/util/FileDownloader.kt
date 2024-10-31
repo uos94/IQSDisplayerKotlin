3939ad4569f9bd5b8c82fdc1891027fb735ea142
HISON_PC\uos94 <uos94se@gmail.com>
2024-10-31 11:01:54 +0900

    사용하지는 않지만 나중에 쓸까봐 추가해둠

---
+++ app/src/main/java/com/kct/iqsdisplayer/util/FileDownloader.kt
@@ -0,0 +1,50 @@
+package com.kct.iqsdisplayer.util
+
+import kotlinx.coroutines.*
+import java.io.File
+import java.net.URL
+import java.nio.ByteBuffer
+import java.nio.channels.Channels
+
+class FileDownloader {
+
+    suspend fun downloadFile(url: String, destinationPath: String, progressCallback: (Float) -> Unit) = withContext(Dispatchers.IO) {
+        try {
+            val connection = URL(url).openConnection()
+            val contentLength = connection.contentLength
+
+            val channel = Channels.newChannel(connection.getInputStream())
+            val outputFile = File(destinationPath)
+            val outputStream = outputFile.outputStream()
+            val fileChannel = outputStream.channel
+
+            var bytesRead = 0L
+            val buffer = ByteArray(DEFAULT_BUFFER_SIZE)
+            var read: Int
+
+            while (channel.read(ByteBuffer.wrap(buffer)).also { read = it } != -1) {
+                fileChannel.write(ByteBuffer.wrap(buffer, 0, read))
+                bytesRead += read
+                val progress = bytesRead.toFloat() / contentLength
+                withContext(Dispatchers.Main) {
+                    progressCallback(progress)
+                }
+            }
+
+            outputStream.close()
+            channel.close()
+
+            withContext(Dispatchers.Main) {
+                progressCallback(1f)
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                throw e
+            }
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_BUFFER_SIZE = 8192
+    }
+}
