6c6308835de875e56113c697b9774117324e5b36
HISON_PC\uos94 <uos94se@gmail.com>
2024-11-08 09:59:17 +0900

    대소문자 정리 추가

--- app/src/main/java/com/kct/iqsdisplayer/data/packet/receive/LastCallData.kt
+++ app/src/main/java/com/kct/iqsdisplayer/data/packet/receive/LastCallData.kt
@@ -2,9 +2,9 @@ package com.kct.iqsdisplayer.data.packet.receive
 
 data class LastCallData(
     /** 발권창구ID */
-    var ticketWinID: Int = 0,
+    var ticketWinId: Int = 0,
     /** 호출창구ID */
-    var callWinID: Int = 0,
+    var callWinId: Int = 0,
     /** 호출창구번호 */
     var callWinNum: Int = 0,
     /** 호출번호 */
@@ -13,7 +13,7 @@ data class LastCallData(
     var waitNum: Int = 0
 ) {
     override fun toString(): String {
-        return "TicketWinID[$ticketWinID], CallWinID[$callWinID], CallWinNum[$callWinNum], CallNum[$callNum], WaitNum[$waitNum]"
+        return "ticketWinId[$ticketWinId], callWinId[$callWinId], callWinNum[$callWinNum], callNum[$callNum], waitNum[$waitNum]"
     }
 }
 
@@ -36,8 +36,8 @@ fun String?.toLastCallList() : ArrayList<LastCallData> {
 
         val item = LastCallData()
 
-        item.ticketWinID   = split[0].toIntOrNull() ?: 0
-        item.callWinID     = split[1].toIntOrNull() ?: 0
+        item.ticketWinId   = split[0].toIntOrNull() ?: 0
+        item.callWinId     = split[1].toIntOrNull() ?: 0
         item.callWinNum    = split[2].toIntOrNull() ?: 0
         item.callNum       = split[3].toIntOrNull() ?: 0
         item.waitNum       = split[4].toIntOrNull() ?: 0
